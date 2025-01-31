12ac77cd9392a271891b3b49137d8a6dee6dec57
Priyanshi Khandelwal <161630817+Priyanshikhandelwal16@users.noreply.github.com>
2025-01-31 17:44:32 +0530

    added

--- js/main.js
+++ js/main.js
@@ -547,3 +547,30 @@
 
 
 })(jQuery);
+
+// Select the slider and container
+
+if ($('.testimonial-slider').length) {
+	const testimonial_slider = new Swiper('.testimonial-slider .swiper', {
+		slidesPerView : 1,
+		speed: 1000,
+		spaceBetween: 30,
+		centeredSlides: true,
+		loop: true,
+		autoplay: {
+			delay: 3000,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		breakpoints: {
+			768:{
+				  slidesPerView: 2,
+			},
+			991:{
+				  slidesPerView: 3,
+			}
+		}
+	});
+}
